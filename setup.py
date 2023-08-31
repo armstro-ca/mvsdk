bb80abcb6339361be851ff2c1f32d15e8efec20d
James Armstrong <j@armstro.ca>
2023-08-30 22:41:39 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mvsdk',
-    version='0.0.4-2308301915',
+    version='0.0.4-2308302245',
     author='James Armstrong',
     author_email='j@armstro.ca',
     description='MediaValet Python SDK',
