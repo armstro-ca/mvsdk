546b6a28a9f8da47d03ba9c7f619b1b2f255ae22
James Armstrong <j@armstro.ca>
2023-08-30 19:11:49 -0700

    Correcting package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='MediaValet SDK',
+    name='mvsdk',
     version='0.0.4',
     author='James Armstrong',
     author_email='j@armstro.ca',
