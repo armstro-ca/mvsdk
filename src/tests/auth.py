fb6107a8cfd8fa05c253896764e19ee3cc953a98
James Armstrong <j@armstro.ca>
2023-10-11 11:59:29 -0700

    Debugging tests

--- src/tests/auth.py
+++ src/tests/auth.py
@@ -1,26 +1,28 @@
 import os
 from mvsdk.rest import Client
 
+from icecream import ic
+
 
 class Auth:
     def __init__(self):
 
-        auth_url = os.getenv('MVAPIAUTHURL')
-        base_url = os.getenv('MVAPIBASEURL')
+        auth_url = ic(os.getenv('MVAPIAUTHURL'))
+        base_url = ic(os.getenv('MVAPIBASEURL'))
 
         self.sdk_handle = Client(auth_url=auth_url, base_url=base_url)
 
-        data = {
+        data = ic({
                 'grant_type': 'password',
                 'username': os.getenv('MVUSERNAME'),
                 'password': os.getenv('MVPASSWORD'),
                 'scope': 'openid api offline_access'
-            }
+            })
 
-        auth = {
+        auth = ic({
                 'client_id': os.getenv('MVCLIENTID'),
                 'client_secret': os.getenv('MVCLIENTSECRET')
-            }
+            })
 
         self.session = self.sdk_handle.connect.auth(
             data=data,
