841dedbf98f7ce7d25021f57bb2875c20f5cb763
James Armstrong <j@armstro.ca>
2023-10-17 21:57:07 -0700

    Implementing export on DirectLinks

--- src/mvsdk/rest/direct_link/__init__.py
+++ src/mvsdk/rest/direct_link/__init__.py
@@ -56,3 +56,26 @@ class DirectLink:
             domain_action=domain_action,
             bulk=bulk
         )
+
+    def export(self, params=None, data=None, headers=None, auth=None, object_id=None,
+               object_action=None, domain_id=None, domain_action='export', stream=True):
+        """
+        https://docs.mediavalet.com/#1e6608b7-5d9a-4904-8ffc-731fc6c4e9c3
+        """
+        headers = headers or {}
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+        return self.mv_sdk.request(
+            'get',
+            self.base_url,
+            self.domain,
+            params=params,
+            data=data,
+            headers=headers,
+            auth=auth,
+            object_id=object_id,
+            object_action=object_action,
+            domain_id=domain_id,
+            domain_action=domain_action,
+            stream=stream
+        )
